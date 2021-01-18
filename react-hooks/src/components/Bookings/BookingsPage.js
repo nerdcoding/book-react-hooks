955f771353cbdbe424528f099945fba6560aadcd
Tobias Koltsch <tobias@koltsch.de>
2021-01-18 08:39:05 +0100

    Add basic React app for chapter 2

---
+++ react-hooks/src/components/Bookings/BookingsPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function BookablesPage() {
+    return (
+        <main className="bookings-page">
+            <p>Bookings!</p>
+        </main>
+    );
+}
