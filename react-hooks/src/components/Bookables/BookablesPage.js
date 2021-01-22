49657dec0f2c626de92a1dbe049a3fbbca1ef027
Tobias Koltsch <tobias@koltsch.de>
2021-01-22 08:21:09 +0100

    Create BookablesList for showing & selecting

--- react-hooks/src/components/Bookables/BookablesPage.js
+++ react-hooks/src/components/Bookables/BookablesPage.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import BookablesList from "./BookablesList";
+
+
 export default function BookablesPage() {
+
     return (
         <main className="bookables-page">
-            <p>Bookables!</p>
+            <BookablesList />
         </main>
     );
 }
