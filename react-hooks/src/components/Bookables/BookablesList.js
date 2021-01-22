1c1151d4102035d9acc1c8240cbfd88bcba500a3
Tobias Koltsch <tobias@koltsch.de>
2021-01-22 14:53:35 +0100

    Use multiple states: add group to state & checkbox to show details

--- react-hooks/src/components/Bookables/BookablesList.js
+++ react-hooks/src/components/Bookables/BookablesList.js
@@ -1,46 +1,106 @@
 import React, {useState} from 'react';
 
-import {bookables} from '../../static.json'
+import {bookables, days, sessions} from '../../static.json'
 import {FaArrowRight} from "react-icons/all";
 
 export default function BookablesList() {
-    const group = 'Rooms';
-    const bookablesInGroup = bookables.filter(b => b.group === group);
+    const [group, setGroup] = useState('Rooms');
+    const [bookableIndex, setBookableIndex] = useState(0);
+    const [showDetails, setShowDetails]  = useState(false);
 
-    const [bookableIndex, setBookableIndex] = useState(1);
+    const groups = [...new Set(bookables.map(bookable => bookable.group))]
+    const bookablesInGroup = bookables.filter(bookable => bookable.group === group);
+    const selectedBookable = bookablesInGroup[bookableIndex];
 
     function changeBookable(selectedIndex) {
         setBookableIndex(selectedIndex);
     }
 
-    function nextBookable(selectedIndex) {
+    function nextBookable() {
         setBookableIndex(i => (i+1) % bookablesInGroup.length);
     }
 
     return (
-        <div>
-            <ul className="bookables items-list-nav">
-                {bookablesInGroup
-                    .map((b, i) =>(
-                        <li key={b.id} className={i === bookableIndex ? "selected" : null}>
-                            <button
-                                className="btn"
-                                onClick={() => changeBookable(i)}>
+        <>
+            <div>
+                <select
+                    value={group}
+                    onChange={(e) => setGroup(e.target.value)}
+                >
+                    {groups.map(g => (
+                        <option key={g} value={g}>{g}</option>
+                    ))}
+                </select>
+
+                <ul className="bookables items-list-nav">
+                    {bookablesInGroup
+                        .map((b, i) =>(
+                            <li key={b.id} className={i === bookableIndex ? "selected" : null}>
+                                <button
+                                    className="btn"
+                                    onClick={() => changeBookable(i)}>
                                     {b.title}
-                            </button>
-                        </li>
-                    ))
-                }
-            </ul>
-            <p>
-                <button
-                    className="btn"
-                    onClick={nextBookable}
-                    autoFocus>
-                    <FaArrowRight />
-                    <span>next</span>
-                </button>
-            </p>
-        </div>
+                                </button>
+                            </li>
+                        ))
+                    }
+                </ul>
+
+                <p>
+                    <button
+                        className="btn"
+                        onClick={nextBookable}
+                        autoFocus>
+                        <FaArrowRight />
+                        <span>next</span>
+                    </button>
+                </p>
+            </div>
+
+            {selectedBookable && (
+                <div className="bookable-details">
+                    <div className="item">
+                        <div className="item-header">
+                            <h2>
+                                {selectedBookable.title}
+                            </h2>
+                            <span className="controls">
+                                <label>
+                                    <input type="checkbox"
+                                           checked={showDetails}
+                                           onChange={() => setShowDetails(oldVal => !oldVal)}
+                                    />
+                                    Show Details
+                                </label>
+                            </span>
+                        </div>
+
+                        <p>{selectedBookable.notes}</p>
+
+                        {showDetails && (
+                            <div className="item-details">
+                                <h3>Availability</h3>
+                                <div className="bookable-availability">
+                                    <ul>
+                                        {selectedBookable.days
+                                            .sort()
+                                            .map(day => <li key={day}>{days[day]}</li>)
+                                        }
+                                    </ul>
+                                    <ul>
+                                        {selectedBookable.sessions
+                                            .sort()
+                                            .map(session => <li key={session}>{sessions[session]}</li>)
+                                        }
+                                    </ul>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            )}
+
+
+        </>
     );
 }
