24160164f63297c4dad3cdfe39aeb94ec558d160
Tobias Koltsch <tobias@koltsch.de>
2021-01-22 14:59:39 +0100

    Update UsersList to show details about the selected user

--- react-hooks/src/components/Users/UsersList.js
+++ react-hooks/src/components/Users/UsersList.js
@@ -1,28 +1,51 @@
 import React, {useState} from 'react';
 
-import {users} from '../../static.json'
+import {days, sessions, users} from '../../static.json'
 
 export default function UsersList() {
-
     const [selectedUserIndex, setSelectedUserIndex] = useState(1);
+    const selectedUser = users[selectedUserIndex];
 
     function changeUser(selectedIndex) {
         setSelectedUserIndex(selectedIndex);
     }
 
     return (
-        <ul className="bookables items-list-nav">
-            {users
-                .map((user, index) =>(
-                    <li key={user.id} className={index === selectedUserIndex ? "selected" : null}>
-                        <button
-                            className="btn"
-                            onClick={() => changeUser(index)}>
-                                {user.name}
-                        </button>
-                    </li>
-                ))
-            }
-        </ul>
-    );
+        <>
+            <ul className="bookables items-list-nav">
+                {users
+                    .map((user, index) =>(
+                        <li key={user.id} className={index === selectedUserIndex ? "selected" : null}>
+                            <button
+                                className="btn"
+                                onClick={() => changeUser(index)}>
+                                    {user.name}
+                            </button>
+                        </li>
+                    ))
+                }
+            </ul>
+
+            {selectedUser && (
+                <div className="bookable-details">
+                    <div className="item">
+                        <div className="item-header">
+                            <h2>
+                                {selectedUser.name}
+                            </h2>
+                        </div>
+
+                        <p>{selectedUser.title}</p>
+
+                        <div className="item-details">
+                            <h3>Availability</h3>
+                            <div className="bookable-availability">
+                                {selectedUser.notes}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
+    )
 }
