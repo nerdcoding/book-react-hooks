ad8c471c9574727fb2a3096177e5b332937e0f09
Tobias Koltsch <tobias@koltsch.de>
2021-01-22 09:32:06 +0100

    Add users into user picker

--- react-hooks/src/components/Users/UserPicker.js
+++ react-hooks/src/components/Users/UserPicker.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import {users} from '../../static.json'
+
 export default function UserPicker() {
     return (
         <select>
-            <option>User Picker</option>
+            {users.map(user => (
+                <option key={user.id}>{user.name}</option>
+            ))}
+
         </select>
     );
 }
