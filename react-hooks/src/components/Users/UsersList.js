428a3dfd63c9e7b793d355eddc907dbdc6876085
Tobias Koltsch <tobias@koltsch.de>
2021-01-26 09:29:51 +0100

    Show errors & spinner when loading users

--- react-hooks/src/components/Users/UsersList.js
+++ react-hooks/src/components/Users/UsersList.js
@@ -1,23 +1,37 @@
 import React, {useState, useEffect} from 'react';
 import axios from "axios";
 import {days, sessions} from '../../static.json'
+import {FaSpinner} from "react-icons/all";
 
 
 export default function UsersList() {
     const [users, setUsers] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
     const [selectedUserIndex, setSelectedUserIndex] = useState(1);
     const selectedUser = users[selectedUserIndex];
 
     useEffect(() => {
         axios.get('http://localhost:3001/users')
-            .then(response =>
-                setUsers(response.data)
-            )
-            .catch(error => console.log(error) )
+            .then(response => {
+                setIsLoading(false);
+                setUsers(response.data);
+            })
+            .catch(error => {
+                setIsLoading(false);
+                setError(error);
+            })
     }, []);
 
     function changeUser(selectedIndex) {
         setSelectedUserIndex(selectedIndex);
+    }
+
+    if (error !== null) {
+        return <p>{error.message}</p>
+    }
+    if (isLoading) {
+        return <p><FaSpinner className="icon-loading" /> Loading users... </p>
     }
 
     return (
